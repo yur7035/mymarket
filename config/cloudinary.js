ae073a849520d5c1acc97d9027e71a464a7b2a69
oesuoehy <choihs1117@gmail.com>
2026-06-13 04:06:20 +0900

    cloudinary 의존성 추가

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,9 @@
 const { v2: cloudinary } = require('cloudinary');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.dzbrxhug8,
+  api_key: process.env.588243894324786,
+  api_secret: process.env.ovKuApQMVaOmGdxU1lIxtHqHt2o
 });
 
 module.exports = cloudinary;
